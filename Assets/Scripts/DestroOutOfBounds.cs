f69476744c3b15dea6d6661663d028bbeac3ace2
nbaramkiazar <naseembaramkiazar@gmail.com>
2020-03-28 00:00:18 -0700

    Unit 1: Independent Project
    unit 1

---
+++ Assets/Scripts/DestroOutOfBounds.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroOutOfBounds : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    private float outOfBoundsY = -3.0f;
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.y >= outOfBoundsY)
+        {
+            Destroy(gameObject);
+        }
+        if (transform.position.y <= outOfBoundsY)
+        {
+            Destroy(gameObject);
+        }
+
+    }
+}
---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] waterdrop;
+    private const float xRange = 10;
+    private const float spawnPointZ = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnWater", 1, 1);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void SpawnWater()
+    {
+        int randomwater = Random.Range(0, waterdrop.Length);
+        Instantiate(waterdrop[randomwater], new Vector3(Random.Range(-xRange, xRange), 0, 25), waterdrop[1].transform.rotation);
+    }
+
+}
